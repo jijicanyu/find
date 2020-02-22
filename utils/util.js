af630d0c9e1137d2076e16debb4cebdb87e3dd58
changlei <769518355@qq.com>
2020-02-22 19:39:41 +0800

    ''

--- utils/util.js
+++ utils/util.js
@@ -52,7 +52,29 @@ function formatTime(number, format) {
   }
   return format;
 }  
+
+/**
+ * 将小程序的API封装成支持Promise的API
+ * @params fn {Function} 小程序原始API，如wx.login
+ */
+const wxPromisify = fn => {
+  return function (obj = {}) {
+    return new Promise((resolve, reject) => {
+      obj.success = function (res) {
+        resolve(res)
+      }
+
+      obj.fail = function (res) {
+        reject(res)
+      }
+
+      fn(obj)
+    })
+  }
+}
+
 module.exports = {
   formatTime: formatTime,
-  regexConfig: regexConfig
+  regexConfig: regexConfig,
+  wxPromisify: wxPromisify
 } 
